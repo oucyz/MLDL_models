24e046c74ec90a0cca797faa307948243cd8049b
oucyz <dks.9886.po@gmail.com>
2021-08-14 02:49:09 +0900

    modified MLP

--- MLP/mlp.py
+++ MLP/mlp.py
@@ -12,11 +12,10 @@ class MLP(nn.Module):
         self.linear1 = nn.Linear(n_in, n_hidden)
         self.relu1 = nn.ReLU()
         self.linear2 = nn.Linear(n_hidden, n_out)
-        self.relu2 = nn.ReLU()
         
     def forward(self, x):
         out = self.relu1(self.linear1(x))
-        out = self.relu2(self.linear2(out))
+        out = self.linear2(out)
         return out
 
 
